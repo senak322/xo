6c5a26a43faeb96b59f77987888e203ba639dc6f
Aleksandr Viznyi <senak98@yandex.ru>
2025-06-18 23:32:19 +0300

    first files

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+// import Image from "next/image";
+
+import { Button } from "@/shared/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button size={"lg"} variant={"ghost"}>Hello world</Button>
+    </div>
+  );
+}

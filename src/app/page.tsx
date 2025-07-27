d8fa2a4c790c118e9ff9b03eb04d50e3704ee8db
Aleksandr Viznyi <senak98@yandex.ru>
2025-07-27 22:32:50 +0300

    add: prisma and docker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 // import Image from "next/image";
 
+import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
 
-export default function Home() {
+export default async function Home() {
+
+  const games = await prisma.game.findMany()
+
+  console.log(games);
+  
+
   return (
     <div>
       <Button size={"lg"} >Hello world</Button>
